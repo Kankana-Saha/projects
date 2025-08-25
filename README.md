c98ee0ab919a284adb4f5d7436cc1e9fed0f241a
Kankana-Saha <142918855+Kankana-Saha@users.noreply.github.com>
2025-08-25 23:54:02 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+# 🏋️ BEAT - Gym Website  
+
+A simple **Gym Website** built using **PHP, MySQL, HTML, CSS**, and **Font Awesome**.  
+This project contains a landing page with services, client testimonials, and a **Contact Us** form that stores data in a MySQL database.  
+
+---
+
+## 🚀 Features
+- 🏠 **Homepage** with navigation bar  
+- 💪 **Our Services** section (Workout, Training, Zumba)  
+- 👥 **Our Clients** showcase  
+- 📩 **Contact Us form** with database integration  
+- 🎨 Responsive design with CSS and media queries  
+- 🔗 Social media links (Facebook, Instagram, LinkedIn, YouTube)  
+
+---
+
+## 🛠️ Tech Stack
+- **Frontend:** HTML, CSS, Font Awesome  
+- **Backend:** PHP  
+- **Database:** MySQL  
+- **Server:** XAMPP / WAMP / LAMP  
+
+---
+
+## 📂 Project Structure
+BEAT-GYM/
+│── index.php # Homepage with services, clients & contact form
+│── about us.php # About page (programs, corporate membership, training)
+│── classes.php # Classes page (timings & packages)
+│── style.css # Inline CSS included, can be externalized
+│── /images # .webp, .jpeg files for site visuals
+│── /database # SQL file for database setup
+│── README.md # Project documentation
---
+++ classes.php
@@ -0,0 +1,371 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gym</title>
+    <STYle>
+        *{
+           
+            margin: 0;
+            padding: 0;
+        }
+        .logo{
+            font-size: 15mm;
+            margin-top: 0.1%;
+            color:brown
+        }
+        nav{
+            display: flex;
+            justify-content: space-around;
+           
+           
+        }
+        nav::before{
+            content: '';
+            background-color: black;
+            position: absolute;
+            height: 8%;
+            width: 100%;
+            z-index: -1;
+            opacity: 8;
+        }
+        nav ul{
+            display: flex;
+            text-decoration: none;
+            justify-content: space-around;
+             margin-top: 1%;
+        }
+       nav ul li {
+            display: flex;
+            padding: 3px 22px;
+            list-style: none;
+            justify-content: space-around;
+           
+
+        }
+        nav ul li a{
+           list-style: none;
+           font-size: 6mm;
+           text-decoration: none;
+           display: flex;
+           color: black;
+
+           
+        }
+        nav ul li a:hover{
+          color: black;
+          background-color: white;
+          border-radius: 30px;
+          margin:0.5% ;
+        }
+        
+        .text a{
+            text-decoration: none;
+            font-size: 6mm;
+            color:white;
+            background-color: brown;
+            margin-top: 1%;
+            
+            
+        }
+        button a{
+            font-size: 6mm;
+            text-decoration: none;
+            color:black;
+            margin-top: 1%;
+        }
+        .home{
+             display: flex;
+             flex-direction: column;
+             margin-top: 3px  ;
+              color: rgb(253, 252, 252);
+              
+             justify-content: center;
+             align-items: center;
+        }
+        .home::before{
+             content: '';
+            background: url("gym3.webp") no-repeat center; 
+            background-size: 100% 100%; 
+            position:absolute;
+            height: 100%;
+            width: 100%;
+            
+            z-index: -1;
+
+            opacity: 0.89;
+           
+        }
+        .b{
+            border: 2px black solid;
+            justify-content: center;
+            margin-right: 400px;
+            margin-left: 400px;
+            
+         }
+       
+        .h1{
+            font-size: 10mm;
+            margin-top: 1%;
+            margin-bottom: 1%;
+        }
+        .p1{
+            font-size: 7mm;
+           
+            margin-bottom: 1%;
+        }
+        .p2{
+            font-size: 7mm;
+           
+            margin-bottom: 1%;
+        }
+        .btn{
+            font-size: 7mm;
+            text-decoration: none;
+           margin-top: 10%;
+           margin-left: 0%;
+            justify-content: center;
+            position: absolute;
+            background-color: brown;
+            color: white;
+            border:2px solid white;
+            cursor: pointer;
+        }
+        ::selection{
+            background-color: antiquewhite;
+            color: black;
+        }
+        
+         
+        .icons{
+            background-color:rgb(196, 92, 28);
+            padding-top: 30px;
+            padding-bottom: 30px;
+        }
+        .socialicons{
+
+           
+            margin: auto;
+            text-align: center;
+        }
+        .footer{
+           margin: auto;
+            color: black;
+            text-align: center;
+        }
+        @media (max-width:1210px){
+           /* nav */
+           nav{
+            flex-direction: column;
+            
+           }
+           nav::before
+           {
+            height: 30%;
+           }
+           .logo
+           {
+            margin: auto;
+           justify-self: center;
+           }
+           nav ul{
+            float: none;
+            display: block;
+            width: 100%;
+            text-align: left;
+           }
+           .text{
+          
+           margin: auto;
+           justify-self: center;
+           
+           }
+           .b{
+            border: 2px black solid;
+            justify-content: center;
+            margin-right: 100px;
+            margin-left: 100px;
+            
+         }
+            /*home  */
+           
+               /* footer */
+               .socialicons{
+                margin: auto;
+           
+           text-align: center;
+            }
+            .footer{
+                margin: auto;
+           
+           text-align: center;
+            }
+        }
+        @media (max-width:448px){
+         /* nav */
+         nav{
+            flex-direction: column;
+            
+           }
+           nav::before
+           {
+            height: 30%;
+           }
+           .logo
+           {
+            margin: auto;
+           justify-self: center;
+           }
+           nav ul{
+            float: none;
+            display: block;
+            width: 100%;
+            text-align: left;
+           }
+           .text{
+          
+           margin: auto;
+           justify-self: center;
+           
+           }
+            /*home  */
+           .home{
+            display: flex;
+            flex-direction: column;
+            margin-top: 20;
+            
+           }
+           .btn{
+            margin-top: 350px;
+            flex-direction: column;
+           }
+           .b{
+            border: 2px black solid;
+            justify-content: center;
+            margin-right: 50px;
+            margin-left: 50px;
+            
+         }
+                   
+            /* services */
+            .h2{
+            padding-top: 40px;
+            margin-left: 30px;
+            font-size: 10mm;
+            display: flex;
+            flex-direction: column;
+        }
+        .ser{ 
+            margin-top: 60px;
+           margin-left: 10px;
+           display: flex;
+           flex-direction: column;
+           
+        }
+        .img1{
+            border: 2px solid black;
+            margin-bottom: 10px;
+            
+        }
+        .img1 img{
+             align-self: baseline;
+            height: 100px;
+            width: 100px;
+           
+        } 
+        .img2 img{
+            align-self: baseline;
+        } 
+        .img3 img{
+            align-self: center;
+            
+        } 
+      
+       
+        .img2{
+            border: 2px solid black;
+            margin-bottom: 10px;
+            
+        }
+        .img3{
+            border: 2px solid black;
+           
+        }
+        .j{
+            align-items: center;
+        }
+        
+            
+               /* footer */
+               .socialicons{
+                margin: auto;
+           
+           text-align: center;
+            }
+            .footer{
+                margin: auto;
+           
+           text-align: center;
+            }
+        }
+    </STYle>
+
+</head>
+<body>
+    <nav style="color: brown;">
+        <div class="logo">BEAT</div>
+        <UL>
+            <li><a href="index.php" ><i class="fa fa-home"></i>HOME</a></li>
+            <li><a href="about us.php"><i class="fa fa-user"></i>ABOUT US</a></li>
+            <li><a href="classes.php"><i class="fa fa-edit"></i>CLASSES</a></li>
+        </UL>
+        <div class="text" style="margin-top: 1%;"><button><a href="#"><i class="fa fa-clone"></i>REGISTER</a></button></div>
+    </nav> <section class="home">
+        <h1 class="h1">Welcome to BEAT</h1>
+        <p class="p1">Motivation is what gets you started.Habit is what keeps you going
+           </p>
+        <p class="p2">We will provide you a good and organised training with wonderful trainers. </p><br>
+           <br>
+        <button class="btn">Become a Member Now</button>
+        </section>
+        <center>
+        <div  class="a" style="margin-top: 200px; padding-bottom: 50px;padding-top: 10px;" >
+            <h1 style="font-size: 10mm; margin-top:100px; ">Classes</h1>
+           
+            <img src="classess.jpg" alt="" style="height: 200px ;width: 200px;">
+            <p class="a" style="margin-top: 50px; "> Our classes will be started at 5AM and end on 10PM. You will get best services here.</p>
+           <h2 style="margin-top: 10px; font-size:10mm;">Packages<h2>
+
+            <ul style=" font-size:4mm;">Gym:
+                <li> Monthly-599Rs.</li>
+                <li> Yearly-6500Rs.</li>
+            </ul>
+            <ul style=" font-size:4mm;">Zumba:
+                <li> Monthly-699Rs.</li>
+                <li> Yearly-7500Rs.</li>
+            </ul>
+            <ul style=" font-size:4mm;">Personal training:
+                <li> Monthly-999Rs.</li>
+                <li> Yearly-1100Rs.</li>
+            </ul>
+        </div>
+       
+       
+  
+   <section class="icons" ">
+    <div class="socialicons">
+        <a href="https://www.facebook.com/kriti.saha.102?mibextid=ZbWKwL"><i class="fa-brands fa-facebook"></i></a>
+        <a href=""><i class="fa-brands fa-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/kankana-saha-072947287?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><i class="fa-brands fa-linkedin"></i></a>
+        <a href="https://youtube.com/@kankanasaha2486?si=0ZFc93wINuIEFxnU"><i class="fa-brands fa-youtube"></i></a>
+    </div>    
+   
+   <div class="footer">
+    <p>copyright &copy;2022;designed by Kankana Saha</p>
+   </div>
+</section>
+</body>
+</html>
